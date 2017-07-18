144cce1acd83d81a16fef2c1c6539357393b3c2b
RandyMuhroji <randyboys81@yahoo.com>
2017-07-18 11:50:56 +0700

    temp

--- database/seeds/DummyTableSeeder.php
+++ database/seeds/DummyTableSeeder.php
@@ -19,6 +19,8 @@ class DummyTableSeeder extends Seeder
      */
     public function run()
     {
+        //Cafe 1
+
     	$user = User::create([
                 'name' => 'Rumah Burger',
                 'email'=> 'rumah@burger.com',
@@ -58,48 +60,48 @@ class DummyTableSeeder extends Seeder
         	[
         		'cafe_id' => $cafe->id,
         		'day' => 0,
-        		'open_hour' => '04:00 pm',
-        		'close_hour' => '11:00 pm',
+        		'open_hour' => '16:00',
+        		'close_hour' => '23:00',
         	]
         );
         DB::table('operational_cafe')->insert(
         	[
         		'cafe_id' => $cafe->id,
         		'day' => 1,
-        		'open_hour' => '11:00 am',
-        		'close_hour' => '11:00 pm',
+        		'open_hour' => '11:00',
+        		'close_hour' => '23:00',
         	]
         );
         DB::table('operational_cafe')->insert(
         	[
         		'cafe_id' => $cafe->id,
         		'day' => 2,
-        		'open_hour' => '11:00 am',
-        		'close_hour' => '11:00 pm',
+        		'open_hour' => '11:00',
+        		'close_hour' => '23:00',
         	]
         );
         DB::table('operational_cafe')->insert(
         	[
         		'cafe_id' => $cafe->id,
         		'day' => 3,
-        		'open_hour' => '11:00 am',
-        		'close_hour' => '11:00 pm',
+        		'open_hour' => '11:00',
+        		'close_hour' => '23:00',
         	]
         );
     	DB::table('operational_cafe')->insert(
         	[
         		'cafe_id' => $cafe->id,
         		'day' => 5,
-        		'open_hour' => '02:00 pm',
-        		'close_hour' => '11:00 pm',
+        		'open_hour' => '14:00',
+        		'close_hour' => '23:00',
         	]
         );
         DB::table('operational_cafe')->insert(
         	[
         		'cafe_id' => $cafe->id,
         		'day' => 6,
-        		'open_hour' => '11:00 am',
-        		'close_hour' => '11:00 pm',
+        		'open_hour' => '11:00',
+        		'close_hour' => '23:00',
         	]
         );
 
@@ -186,6 +188,19 @@ class DummyTableSeeder extends Seeder
         	]
     	);
 
+        DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Teh manis dingin',
+                'price'     => '6000',
+                'images'    => 'burger/es-teh-manis.jpg.jpg',
+                'tag'       => 'Halal, Burger',
+                'category'  => 'Minuman',
+                'status'    => 1,
+                'desc'      => 'Teh manis dingin'
+            ]
+        );
+
         $album = AlbumGallery::create(
             [
                 'user_id' =>  $user->id,
@@ -212,6 +227,424 @@ class DummyTableSeeder extends Seeder
         		'status'	=> 1
         	]
     	);
+
+        //cafe2
+
+        $user = User::create([
+                'name' => 'Ayam Penyet Rahmat',
+                'email'=> 'ayampenyet@rahmat.com',
+                'password' => bcrypt('owner'),
+                'status' => 1,
+            ]);
+
+        $role = Role::find(2);
+        $user->attachRole($role);
+
+        $address = 'Jl. Letda Sudjono, Bandar Selamat, Medan Tembung, Kota Medan, Sumatera Utara 20223, Indonesia';
+        $arrAddress = explode(',',$address);
+
+        $kec = $arrAddress[2];
+        $kel = $arrAddress[1];
+
+
+        $cafe = Cafe::create(
+            [
+                'user_id' => $user->id,
+                'name' => 'Ayam Penyet Rahmat',
+                'address' => $address,
+                'phone' => '061-7331875',
+                'lat' => '3.597696',
+                'long' => '98.71657600000003',
+                'kecamatan' => $kec,
+                'kelurahan' => $kel,
+                'rating' => 0,
+                'status' => 1,
+                'seat' => 120,
+                'web' => 'http://ayampenyetrahmat.com',
+                'facebook' => 'http://facebook.com/ayampenyetrahmat',
+                'twitter' => 'http://twitter.com/ayampenyetrahmat',
+                'linkedin' => 'http://linkedin.com/ayampenyetrahmat',
+            ]
+        );
+
+        DB::table('operational_cafe')->insert(
+            [
+                'cafe_id' => $cafe->id,
+                'day' => 0,
+                'open_hour' => '10:00',
+                'close_hour' => '23:00',
+            ]
+        );
+        DB::table('operational_cafe')->insert(
+            [
+                'cafe_id' => $cafe->id,
+                'day' => 1,
+                'open_hour' => '10:00',
+                'close_hour' => '23:00',
+            ]
+        );
+        DB::table('operational_cafe')->insert(
+            [
+                'cafe_id' => $cafe->id,
+                'day' => 2,
+                'open_hour' => '10:00',
+                'close_hour' => '23:00',
+            ]
+        );
+        DB::table('operational_cafe')->insert(
+            [
+                'cafe_id' => $cafe->id,
+                'day' => 3,
+                'open_hour' => '10:00',
+                'close_hour' => '23:00',
+            ]
+        );
+        DB::table('operational_cafe')->insert(
+            [
+                'cafe_id' => $cafe->id,
+                'day' => 4,
+                'open_hour' => '10:00',
+                'close_hour' => '23:00',
+            ]
+        );
+        DB::table('operational_cafe')->insert(
+            [
+                'cafe_id' => $cafe->id,
+                'day' => 5,
+                'open_hour' => '13:30',
+                'close_hour' => '23:00',
+            ]
+        );
+        DB::table('operational_cafe')->insert(
+            [
+                'cafe_id' => $cafe->id,
+                'day' => 6,
+                'open_hour' => '11:00',
+                'close_hour' => '23:00',
+            ]
+        );
+
+        DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Ayam Penyet Sambal Pedas',
+                'price'     => '13000',
+                'images'    => 'APrahmat/Resep-Ayam-Penyet-dan-Sambal-Pedas-Lezat.png',
+                'tag'       => 'Ayam Penyet, Halal',
+                'category'  => 'Makanan',
+                'status'    => 1,
+            ]
+        );
+        DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Ayam Penyet Sambal Ijo',
+                'price'     => '13000',
+                'images'    => 'APrahmat/resep-ayam-penyel-sambal-cabai-hijau.jpg',
+                'tag'       => 'Halal, Ayam Penyet',
+                'category'  => 'Makanan',
+                'status'    => 1,
+            ]
+        );
+       DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Ayam Penyet Komplit mantap',
+                'price'     => '32000',
+                'images'    => 'APrahmat/resep-ayam-penyel-sambal-cabai-hijau.jpg',
+                'tag'       => 'Halal, Ayam Penyet',
+                'category'  => 'Makanan',
+                'status'    => 1,
+                'desc'      => 'Nasi + Ayam Penyet + Lalapan + Minuman Pilihan'
+            ]
+        );
+        DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Ayam Penyet Komplit sederhna',
+                'price'     => '25000',
+                'images'    => 'APrahmat/resep-ayam-penyel-sambal-cabai-hijau.jpg',
+                'tag'       => 'Halal, Ayam Penyet',
+                'category'  => 'Makanan',
+                'status'    => 1,
+                'desc'      => 'Nasi + Ayam Penyet + Lalapan + Teh Manis Dingin'
+            ]
+        );
+        DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Ice choco float',
+                'price'     => '15000',
+                'images'    => 'APrahmat/ombe cafe.jpg',
+                'tag'       => 'Jus, float',
+                'category'  => 'Minuman',
+                'status'    => 1,
+            ]
+        );
+        DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Ice Drink ',
+                'price'     => '13000',
+                'images'    => 'APrahmat/ice-drink-4.jpg',
+                'tag'       => 'jus',
+                'category'  => 'Minuman',
+                'status'    => 1,
+            ]
+        );
+        DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Teh Manis Dingin',
+                'price'     => '6000',
+                'images'    => 'APrahmat/es-teh-manis.jpg',
+                'tag'       => 'Mandi',
+                'category'  => 'Makanan',
+                'status'    => 1,
+            ]
+        );
+
+        $album = AlbumGallery::create(
+            [
+                'user_id' =>  $user->id,
+                'name' => 'slider'
+            ]
+        );
+
+        DB::table('gallery')->insert(
+            [
+                'user_id'   => $user->id,
+                'album_id'  => $album->id,
+                'filename'  => 'APrahmat/resep-ayam-penyel-sambal-cabai-hijau.jpg',
+                'title'     => 'Ayam Penyet Sambal Hijau',
+                'status'    => 1
+            ]
+        );
+
+        DB::table('gallery')->insert(
+            [
+                'user_id'   => $user->id,
+                'album_id'  => $album->id,
+                'filename'  => 'APrahmat/foody-ayam-goreng.jpg',
+                'title'     => 'View Ayam Penyet Rahmat',
+                'status'    => 1
+            ]
+        );
+
+        DB::table('gallery')->insert(
+            [
+                'user_id'   => $user->id,
+                'album_id'  => $album->id,
+                'filename'  => 'APrahmat/12825945_1270302919663658_1350279316_n.jpg',
+                'title'     => 'View Ayam Penyet Rahmat',
+                'status'    => 1
+            ]
+        );
+
+        //cafe3
+
+        $user = User::create([
+                'name' => 'Kopi Sadis',
+                'email'=> 'kopi@sadis.com',
+                'password' => bcrypt('owner'),
+                'status' => 1,
+            ]);
+
+        $role = Role::find(2);
+        $user->attachRole($role);
+
+        $address = 'Jl. Nusantara No.5, Hutan, Percut Sei Tuan, Kota Medan, Sumatera Utara 20371, Indonesia';
+        $arrAddress = explode(',',$address);
+
+        $kec = $arrAddress[2];
+        $kel = $arrAddress[1];
+
+
+        $cafe = Cafe::create(
+            [
+                'user_id' => $user->id,
+                'name' => 'Kopi Sadis',
+                'address' => $address,
+                'phone' => '061-7331875',
+                'lat' => '3.597525',
+                'long' => '98.71657600000003',
+                'kecamatan' => $kec,
+                'kelurahan' => $kel,
+                'rating' => 0,
+                'status' => 1,
+                'seat' => 60,
+                'web' => 'http://kopisadis.com',
+                'facebook' => 'http://facebook.com/kopisadis',
+                'twitter' => 'http://twitter.com/kopisadis',
+                'linkedin' => 'http://linkedin.com/kopisadis',
+            ]
+        );
+
+        DB::table('operational_cafe')->insert(
+            [
+                'cafe_id' => $cafe->id,
+                'day' => 0,
+                'open_hour' => '16:00',
+                'close_hour' => '24:00',
+            ]
+        );
+        DB::table('operational_cafe')->insert(
+            [
+                'cafe_id' => $cafe->id,
+                'day' => 1,
+                'open_hour' => '14:00',
+                'close_hour' => '23:00',
+            ]
+        );
+        DB::table('operational_cafe')->insert(
+            [
+                'cafe_id' => $cafe->id,
+                'day' => 2,
+                'open_hour' => '14:00',
+                'close_hour' => '23:00',
+            ]
+        );
+        DB::table('operational_cafe')->insert(
+            [
+                'cafe_id' => $cafe->id,
+                'day' => 3,
+                'open_hour' => '14:00',
+                'close_hour' => '23:00',
+            ]
+        );
+        DB::table('operational_cafe')->insert(
+            [
+                'cafe_id' => $cafe->id,
+                'day' => 5,
+                'open_hour' => '14:00',
+                'close_hour' => '23:00',
+            ]
+        );
+        DB::table('operational_cafe')->insert(
+            [
+                'cafe_id' => $cafe->id,
+                'day' => 6,
+                'open_hour' => '14:00',
+                'close_hour' => '23:00',
+            ]
+        );
+
+        DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Crispy Bakso',
+                'price'     => '13000',
+                'images'    => 'kopisadis/P_20160113_180741_BF-640x404.jpg',
+                'tag'       => 'Bakso bakar, Halal, Crispy',
+                'category'  => 'Makanan',
+                'status'    => 1,
+            ]
+        );
+        DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Ayam Penyet Sambal Ijo',
+                'price'     => '13000',
+                'images'    => 'APrahmat/resep-ayam-penyel-sambal-cabai-hijau.jpg',
+                'tag'       => 'Halal, Ayam Penyet',
+                'category'  => 'Makanan',
+                'status'    => 1,
+            ]
+        );
+       DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Ayam Penyet Komplit mantap',
+                'price'     => '32000',
+                'images'    => 'APrahmat/resep-ayam-penyel-sambal-cabai-hijau.jpg',
+                'tag'       => 'Halal, Ayam Penyet',
+                'category'  => 'Makanan',
+                'status'    => 1,
+                'desc'      => 'Nasi + Ayam Penyet + Lalapan + Minuman Pilihan'
+            ]
+        );
+        DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Ayam Penyet Komplit sederhna',
+                'price'     => '25000',
+                'images'    => 'APrahmat/resep-ayam-penyel-sambal-cabai-hijau.jpg',
+                'tag'       => 'Halal, Ayam Penyet',
+                'category'  => 'Makanan',
+                'status'    => 1,
+                'desc'      => 'Nasi + Ayam Penyet + Lalapan + Teh Manis Dingin'
+            ]
+        );
+        DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Ice choco float',
+                'price'     => '15000',
+                'images'    => 'APrahmat/ombe cafe.jpg',
+                'tag'       => 'Jus, float',
+                'category'  => 'Minuman',
+                'status'    => 1,
+            ]
+        );
+        DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Ice Drink ',
+                'price'     => '13000',
+                'images'    => 'APrahmat/ice-drink-4.jpg',
+                'tag'       => 'jus',
+                'category'  => 'Minuman',
+                'status'    => 1,
+            ]
+        );
+        DB::table('menu_cafe')->insert(
+            [
+                'cafe_id'   => $cafe->id,
+                'name'      => 'Teh Manis Dingin',
+                'price'     => '6000',
+                'images'    => 'APrahmat/es-teh-manis.jpg',
+                'tag'       => 'Mandi',
+                'category'  => 'Makanan',
+                'status'    => 1,
+            ]
+        );
+
+        $album = AlbumGallery::create(
+            [
+                'user_id' =>  $user->id,
+                'name' => 'slider'
+            ]
+        );
+
+        DB::table('gallery')->insert(
+            [
+                'user_id'   => $user->id,
+                'album_id'  => $album->id,
+                'filename'  => 'APrahmat/resep-ayam-penyel-sambal-cabai-hijau.jpg',
+                'title'     => 'Ayam Penyet Sambal Hijau',
+                'status'    => 1
+            ]
+        );
+
+        DB::table('gallery')->insert(
+            [
+                'user_id'   => $user->id,
+                'album_id'  => $album->id,
+                'filename'  => 'APrahmat/foody-ayam-goreng.jpg',
+                'title'     => 'View Ayam Penyet Rahmat',
+                'status'    => 1
+            ]
+        );
+
+        DB::table('gallery')->insert(
+            [
+                'user_id'   => $user->id,
+                'album_id'  => $album->id,
+                'filename'  => 'APrahmat/12825945_1270302919663658_1350279316_n.jpg',
+                'title'     => 'View Ayam Penyet Rahmat',
+                'status'    => 1
+            ]
+        );
 
     }
 }
